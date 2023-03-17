096f8cc825655953394dbd3db6485ac8f59c4cb5
vinicius <v.soares.lisboa@gmail.com>
2023-03-17 05:58:27 -0300

    update styles

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -2,24 +2,24 @@ import { useContext } from 'react';
 import { Exit } from '../../assets/Exit';
 import { MenuContext } from '../../context/MenuContext';
 import { NavLinks } from '../NavLinks';
-import { MenuContainer, MenuHeader } from './styles';
+import { Container, MenuHeader } from './styles';
 
 
 export function AsideMenu() {
   const { isMenuOpen, handleToggleMenuOpen } = useContext(MenuContext)
 
   return (
-    <MenuContainer isMenuOpen={isMenuOpen}>
-      <div className='div-aside'>
-        <MenuHeader>
-          <h1>Golden</h1>
-          <Exit onClick={handleToggleMenuOpen} />
-        </MenuHeader>
+    <Container isMenuOpen={isMenuOpen}>
+      <MenuHeader>
+        <h1>Golden</h1>
+        <Exit onClick={handleToggleMenuOpen} />
+      </MenuHeader>
 
+      <div className="div-actions-menu">
         <NavLinks direction='column' />
 
         <button className='primary'>Entrar</button>
       </div>
-    </MenuContainer>
+    </Container>
   );
 }
--- src/components/AsideMenu/styles.ts
+++ src/components/AsideMenu/styles.ts
@@ -4,8 +4,8 @@ interface MenuContainerProps {
   isMenuOpen: boolean;
 }
 
-export const MenuContainer = styled.aside<MenuContainerProps>`
-  width: 250px;
+export const Container = styled.aside<MenuContainerProps>`
+  width: 280px;
   min-height: 100%;
 
   position: fixed;
@@ -18,30 +18,37 @@ export const MenuContainer = styled.aside<MenuContainerProps>`
   transform: ${({ isMenuOpen }) => isMenuOpen ? 'translateX(0)' : 'translateX(-100%)'};
   transition: .3s ease-in-out;
 
-
-  .div-aside {
+  .div-actions-menu {
     padding: 24px;
+
     .primary {
-      width: 100%;
-      padding: 16px;
-      border-radius: 4px;
-      border: none;
-      background-color: #333;
-      color: #fff;
-      cursor: pointer;
-  }
-    h1 {
-      font-size: 32px;
-      margin: 0;
+        width: 100%;
+        padding: 16px;
+        border-radius: 4px;
+        border: none;
+        background-color: #333;
+        color: #fff;
+        cursor: pointer;
     }
+  }
+
+  h1 {
+    font-size: 32px;
+    margin: 0;
+
+    color: #fff;
   }
 `;
 
 export const MenuHeader = styled.div`
+  padding: 24px;
+
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 32px;
+
+  background-color: #333;
+
   button {
     border: none;
     cursor: pointer;
